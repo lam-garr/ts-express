a5ca092f1e0d9678e959b3804288c027933d8446
Garrett <lamgarr@outlook.com>
2022-12-12 19:05:03 -0800

    Add express

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+import express, {Express, Request, Response} from "express";
+
+const app: Express = express();
+
+const port = 3000;
+
+app.get("/",(req: Request,res: Response)=>{
+    res.send("Hello World")
+})

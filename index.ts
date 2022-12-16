35b8da5aa008070f6d9e75a0e8754296e243e185
Garrett <lamgarr@outlook.com>
2022-12-15 19:33:37 -0800

    Add database connection

--- index.ts
+++ index.ts
@@ -9,6 +9,11 @@ dotenv.config();
 
 const LocalStrategy = passportLocal.Strategy;
 
+const mongoDB = `${process.env.MONGODB}`;
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "db connection error"));
+
 const app: Express = express();
 
 app.use(express.json());

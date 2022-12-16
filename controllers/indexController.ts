7443bdf259cb66d8b66945b7432a0aada094c8db
Garrett <lamgarr@outlook.com>
2022-12-15 20:46:17 -0800

    Add passport authentication

--- controllers/indexController.ts
+++ controllers/indexController.ts
@@ -1,4 +1,7 @@
-import express, {Request, Response} from "express"
+import express, {Request, Response, NextFunction} from "express"
+import User from "../models/users";
+import bcrypt from "bcrypt";
+import passport from "passport";
 
 export function get_index(req:Request, res:Response){
     res.send("Hello World");
@@ -6,4 +9,51 @@ export function get_index(req:Request, res:Response){
 
 export function get_other(req: Request, res:Response){
     res.send("Other")
+}
+
+export async function post_sign_up(req: Request, res: Response, next: NextFunction){
+    const hasedpw = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+        username: req.body.username,
+        password: hasedpw
+    }).save(err => {
+        if(err){
+            return next(err);
+        }
+        res.json({signup:"success"})
+    })
+}
+
+export function log_in(req:Request, res: Response, next: NextFunction){
+    passport.authenticate("local", (err, user, info) => {
+        if(err){
+            return next(err);
+        }
+
+        if(!user){
+            return res.json({message:"user not found"});
+        }
+
+        if(info){
+            return res.json({message:`${info.message}`});
+        }
+
+        req.login(user, (err)=>{
+            if(err){
+                return next(err);
+            }
+
+            return res.json({message:`user logged in as ${user.id}`})
+        })
+    })(req, res, next);
+}
+
+export function log_out(req: Request, res: Response, next: NextFunction){
+    req.logout(function (err){
+        if(err){
+            return next(err);
+        }
+        res.json({message:"logged out"})
+    })
 }
--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { builtinModules } from "module";
-import {get_index, get_other} from "../controllers/indexController";
+import {get_index, get_other, post_sign_up, log_in, log_out} from "../controllers/indexController";
 
 const router = express.Router();
 
 router.get("/", get_index);
 
 router.get("/other", get_other)
+
+router.post('sign-up', post_sign_up);
+
+router.post('log-in', log_in);
+
+router.get('log-out', log_out)
 
 export default router;
